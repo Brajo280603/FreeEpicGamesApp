87730262591177426ed0fbce34456fffc4437f12
Brajananda Mahata <brajanandamahato280603@gmail.com>
2023-02-25 12:57:42 +0530

    full offline commit

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'offline';
-const OFFLINE_URL = './index.html';
+const OFFLINE_URL = './offline.html';
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
